b1a5fb25e9eaf7ad025c6b2d77c4d1f0196680c4
vaibs107 <vhongal@hawk.iit.edu>
2018-05-21 00:01:56 -0500

    Completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mm = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var sh = new StickyHeader();
